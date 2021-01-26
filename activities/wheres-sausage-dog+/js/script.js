971b3995de235309672a5cb8b85268975fdf831c
MC <larivieremc@gmail.com>
2021-01-26 16:13:14 -0500

    A1: create and display animal object

--- activities/wheres-sausage-dog+/js/script.js
+++ activities/wheres-sausage-dog+/js/script.js
@@ -1,35 +1,65 @@
 /*****************
 
-Title of Project
-Author Name
+Where's Sausage Dog
+MC Lariviere
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+
+This is an activity in cart 263 presented to Pippin Barr & Dana
+It was previously explored here: https://github.com/lx-muse/cart253/tree/master/exercice3
+Some elements stayed, yet the whole program was rewritten for 2021
+p5 librairy was a little rusty
+Has it been 16 months already ?!
 
 ******************/
+"use strict";
+
+//declaring global variables for the whole program
+const NUM_ANIMAL_IMAGES = 10;
+const NUM_ANIMALS = 100;
+//I understand this layer now
+let animalImages = [];
+let animals = [];
 
 // preload()
-//
-// Description of preload
+// for loop to load and push animal images in the array
+// Using backtick`` means template strings and will +1 to the name, replacing ${i}
 
 function preload() {
-
+  for(let i = 0; i < NUM_ANIMAL_IMAGES; i++){
+    let animalImage = loadImage(`assets/images/animal${i}.png`);
+    animalImages.push(animalImage);
+  }
 }
 
 
 // setup()
 //
-// Description of setup
+// for 100 random images at random position
 
 function setup() {
-
+  createCanvas(windowWidth, windowHeight);
+  for(let i = 0; i < NUM_ANIMALS; i++) {
+    //from the animal constructor
+    let x = random(0, width);
+    let y = random(0, height);
+    let animalImage = random(animalImages);
+    // create an animal
+    let animal = new Animal(x, y, animalImage);
+    //push the array of animals to create many
+    animals.push(animal);
+  }
 }
-
 
 // draw()
 //
-// Description of draw()
+// will display the animals
 
 function draw() {
+  background(0, 125, 255);
+  //from the array of animals
+  for(let i = 0; i < animals.length; i++) {
+    //update method from the annimal object
+    animals[i].update();
 
+  }
 }
