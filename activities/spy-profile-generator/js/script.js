3cf1ab33de74c1e78d8c45006a693785fd9752e7
MC <larivieremc@gmail.com>
2021-02-07 21:14:16 -0500

    A3: started project

---
+++ activities/spy-profile-generator/js/script.js
@@ -0,0 +1,37 @@
+/*****************
+
+Spy Pofile Generator
+MC Lariviere in cart 263
+
+
+
+A program that generates profiles with JSON
+******************/
+let spyProfile = {
+  name: `*redacted*`,
+
+};
+
+
+
+// Description of preload
+
+function preload() {
+
+}
+
+
+
+// Description of setup
+
+function setup() {
+
+}
+
+
+
+// Description of draw()
+
+function draw() {
+
+}
