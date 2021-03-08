4f4bf6d7dce204c56cb77bcd89e6280be7f59344
MC <larivieremc@gmail.com>
2021-03-07 20:54:02 -0500

    A5 : Line1P is undefined

--- activities/haiku-generator/js/script.js
+++ activities/haiku-generator/js/script.js
@@ -15,7 +15,7 @@ let fiveSyllableLines = [
   "They are all gone now"
 ];
 let sevenSyllableLines =[
-"  Say the things left unsaid",
+"Say the things left unsaid",
 "Never believe the wind's lies",
 "The autumn stretches its legs",
 "Nothing can satisfy you",
@@ -26,10 +26,33 @@ let line1 = random(fiveSyllableLines);
 let line2 = random(sevenSyllableLines);
 let line3 = random(fiveSyllableLines);
 
-console.log(line1 + line2 + line3);
+console.log(line1 + line2 +  line3);
 
-function setup() {
+let line1P = document.getElementById(`line-1`);
+let line2P = document.getElementById(`line-2`);
+let line3P = document.getElementById(`line-3`);
 
+console.log(line1P);
+
+line1P.innerText = line1;
+line2P.innerText = line2;
+line3P.innerText = line3;
+
+line1P.addEventListener(`click`, lineClicked);
+line2P.addEventListener(`click`, lineClicked);
+line3P.addEventListener(`click`, lineClicked);
+
+function lineClicked(event){
+  setNewLine(event.target);
+}
+
+function setNewLine(element) {
+  if(element === line1P || element === line3P){
+    element.innerText = random(fiveSyllableLines);
+  }
+  else if(element === line2P){
+    element.innerText = random(sevenSyllableLines);
+  }
 }
 
 function random(array){
