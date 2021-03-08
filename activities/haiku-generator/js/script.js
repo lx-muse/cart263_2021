5098a8f065410b8b787f497b3d01a2624098fd00
MC <larivieremc@gmail.com>
2021-03-07 19:25:42 -0500

    A5: added poem lines

--- activities/haiku-generator/js/script.js
+++ activities/haiku-generator/js/script.js
@@ -5,7 +5,34 @@ MC Lariviere in cart 263
 A program that generates poems
 
 ******************/
+"use strict";
+
+let fiveSyllableLines = [
+  "O, to be a tree",
+  "The cat does not know",
+  "We are all forests",
+  "You have done your best",
+  "They are all gone now"
+];
+let sevenSyllableLines =[
+"  Say the things left unsaid",
+"Never believe the wind's lies",
+"The autumn stretches its legs",
+"Nothing can satisfy you",
+"They will not come back again"
+];
+
+let line1 = random(fiveSyllableLines);
+let line2 = random(sevenSyllableLines);
+let line3 = random(fiveSyllableLines);
+
+console.log(line1 + line2 + line3);
 
 function setup() {
 
+}
+
+function random(array){
+  let index = Math.floor(Math.random() * array.length);
+  return array[index];
 }
