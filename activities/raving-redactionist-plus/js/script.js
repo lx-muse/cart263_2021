95e39e4b9ade9179a4623b3eccc2c28102d2c162
MC <larivieremc@gmail.com>
2021-03-15 22:51:27 -0400

    A6: added Pippin's template

--- activities/raving-redactionist-plus/js/script.js
+++ activities/raving-redactionist-plus/js/script.js
@@ -1,35 +1,63 @@
-/*****************
+/**
+Raving Redactionist
+an acivity from Pippin Barr
+You are redacting a document, but it keeps becoming unredacted!
+Click the secret information to hide it, don`t let all the
+secrets become revealed!
+*/
 
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
-
-******************/
-
-// preload()
-//
-// Description of preload
-
-function preload() {
-
-}
+"use strict";
 
 
-// setup()
-//
-// Description of setup
+// The chance a span will be revealed per update
+const REVEAL_PROBABILITY = 0.1;
+// How often to update the spans (potentially revealing them)
+const UPDATE_FREQUENCY = 500;
 
+// A place to store the jQuery selection of all secrets
+let $secrets;
+
+setup();
+
+/**
+Sets the click handler and starts the time loop
+*/
 function setup() {
+  // Save the selection of all secrets (since we do stuff to them multiple times)
+  $secrets = $(`.secret`);
+  // Set a click handler on the secrets (so we know when they`re clicked)
+  $secrets.on(`click`, redact);
+  // Set an interval of 500 milliseconds to attempt the revelation of secrets
+  setInterval(revelation, UPDATE_FREQUENCY);
+};
 
+/**
+When a secret is clicked we remove its revealed class and add the redacted class
+thus blacking it out
+*/
+function redact() {
+  $(this).removeClass(`revealed`);
+  $(this).addClass(`redacted`);
 }
 
+/**
+Update is called every 500 milliseconds and it updates all the secrets on the page
+using jQuery`s each() function which calls the specified function on _each_ of the
+elements in the selection
+*/
+function revelation() {
+  $secrets.each(attemptReveal);
+}
 
-// draw()
-//
-// Description of draw()
-
-function draw() {
-
+/**
+With random chance it unblanks the current secret by removing the
+redacted class and adding the revealed class. Because this function is called
+by each(), "this" refers to the current element that each has selected.
+*/
+function attemptReveal() {
+  let r = Math.random();
+  if (r < REVEAL_PROBABILITY) {
+    $(this).removeClass(`redacted`);
+    $(this).addClass(`revealed`);
+  }
 }
