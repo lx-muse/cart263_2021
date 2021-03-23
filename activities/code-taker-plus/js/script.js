5ea36a6baee2c6b6e850056ee9ac3040aca4fd84
MC <larivieremc@gmail.com>
2021-03-22 20:49:58 -0400

    A7: droppable

--- activities/code-taker-plus/js/script.js
+++ activities/code-taker-plus/js/script.js
@@ -7,6 +7,20 @@ game stuff
 ******************/
 "use strict";
 
-$(".secret").on('mouseover', function(event){
+$(".secret").one('mouseover', function(event) {
   $(this).addClass('found', 500);
+  $(this).draggable({
+    helper: 'clone'
+  });
+});
+
+$('#answer').droppable({
+  drop: function(event, ui) {
+    // catch the letter in a jquery ui object and append its text to the box section
+    let letter = ui.draggable.text();
+    $(this).append(letter);
+    // refers to the ui object (letters).draggable method
+    ui.draggable.dragabble(`disable`);
+    ui.dragabble.removeClass(`found`);
+  }
 });
