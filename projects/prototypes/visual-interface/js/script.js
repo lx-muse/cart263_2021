4f1737360a62c83154c4b010985a7720728d113d
MC <larivieremc@gmail.com>
2021-03-29 20:41:41 -0400

    P2-added template

---
+++ projects/prototypes/visual-interface/js/script.js
@@ -0,0 +1,12 @@
+/*****************
+
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+
+******************/
+
+
+"use strict";
